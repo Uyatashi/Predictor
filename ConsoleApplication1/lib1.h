a9e2ce827179c0aa423aedb2dddf8fed7639d0f1
Uyatashi <uyatashi@Gmail.com>
2014-10-02 14:38:41 +0300

    Parser v1.3 & 1bitPredictor(&file writer)

--- ConsoleApplication1/lib1.h
+++ ConsoleApplication1/lib1.h
@@ -1,27 +1,54 @@
 #include <string.h>
-#include <stdlib.h>
+#include <stdio.h>
 
-int checkhex(const char* hex)
+
+void bit1w(int a, FILE *temp);
+
+int parser(char *str, FILE *in)
+{
+	int c;
+	int i = 0;
+	while ((c = fgetc(in)) != 10)
+	{ 
+		if ( c == -1 ) { return(0); }
+		str[i] = c; 
+		i++;
+	}
+	return(1);
+}
+
+int checkhex(char* hex)
 {
 	int i;
 	for( i = 0; i < 8; i++ )
 	{ 
+		//printf("%c", hex[i]);
 		if (!((hex[i] >= 48 && hex[i] <= 57) || (hex[i] >= 65 && hex[i] <= 70))) { return(0); }
 	}
 	return(1);
 }
 
-void branchreport(int bcount, int icount) { printf("Branch : %d | Iterations : %d\n", bcount+1, icount); }
+void branchreport(int bcount, int icount) { printf("Branch : %d | Iterations : %d\n", bcount, icount); }
 
+void initbit1(FILE *temp) { fprintf(temp,"1-bit Predictor\nPrediction\tAction\n"); }
 
-/*int bit1(int pr, int s)
+void initbit2(FILE *temp) { fprintf(temp,"2-bit Predictor\nPrediction\tAction\tPrediction\tAction\n"); }
+
+int bit1(int pr, int s, FILE *temp)
 {
-	if ((pr == 1) && (s == 1))	{ bit1w(3); return(1); }
-	else if ((pr == 1) && (s == 0)) { bit1w(2); return(0); }
-	else if ((pr == 0) && (s == 1)) { bit1w(1); return(1); }
-	else { 1bitpredictor(0); return(0); }
-} */
+	if ((pr == 1) && (s == 1))	{ bit1w(3, temp); return(1); }
+	else if ((pr == 1) && (s == 0)) { bit1w(2, temp); return(0); }
+	else if ((pr == 0) && (s == 1)) { bit1w(1, temp); return(1); }
+	else { bit1w(0, temp); return(0); }
+}
 
-//void bit1w(int a)
-//{
-	
+void bit1w(int a, FILE *temp)
+{
+	switch(a)
+	{
+		case 0:	{ fprintf(temp,"NT\t\tNT (Hit)\n"); break; }
+		case 1: { fprintf(temp,"NT\t\tT\n"); break; }
+		case 2: { fprintf(temp,"T\t\tNT\n"); break; }
+		case 3: { fprintf(temp,"T\t\tT (Hit)\n"); }
+	}
+}
--- ConsoleApplication1/main.c
+++ ConsoleApplication1/main.c
@@ -7,20 +7,21 @@
 #include <string.h>
 #include "lib1.h"
 
-
 int main( int argc, char *argv[] )
 {
-	FILE *input1;
-	char tempstr[9];
+	FILE *input1, *out1, *out2;
+	char tempstr[8];
 	int flag = 0;
 	int state;
 	int prediction1 = 1;
-	int prediction2 = 1;
+	int prediction2 = 3;
 	int bc = 0;
-	int ic = 0;
+	int ic[2];
 	int init = 0;
 	long lid[2];
 	unsigned long tempnum;
+	ic[0] = 0;
+	ic[1] = 0;
 	if ( argc != 2 )
 	{
 		printf("Wrong number of arguments. Please re-run the program.\n");
@@ -29,71 +30,74 @@ int main( int argc, char *argv[] )
 	}
 	input1 = fopen(argv[1], "r");
 	if (input1 == NULL) {perror("Error opening file\n"); return(2);}
-	fopen("1bitPredictionTable.txt", "w");
-	fopen("2bitPredictionTable.txt", "w");
-	while(fgets(tempstr, 9,input1) != NULL)
+	out1 = fopen("1bitPredictionTable.txt", "w");
+	out2 = fopen("2bitPredictionTable.txt", "w");
+	initbit1(out1);
+	initbit2(out2);
+	while(parser(tempstr, input1) != 0)
 	{
-		puts(tempstr);
-		/*if (checkhex(tempstr) == 0)
+		if (checkhex(tempstr) == 0)
 		{
 				printf("Wrong address. Please provide a valid input file.\n");
 				system("pause");
 				return(-1);
-		}*/
+		}
 		tempnum = strtol(tempstr, NULL, 16);
-		printf("%lu\n", tempnum);
 		if (init == 1)
 		{
 			if (flag == 0)
 			{
 				if (tempnum == lid[0])
 				{
-					ic++;
+					ic[0]++;
 					state = 1;
 				}
 				else
 				{
-					branchreport(bc, ic);
 					lid[1] = tempnum;
 					state = 0;
 					bc++;
 					flag++;
-					ic = 1;
+					ic[1] = 1;
 				}
 			}
 			else
 			{
 				if (tempnum == lid[1])
 				{
-					ic++;
+					ic[1]++;
 					state = 1;
 				}
 				else if (tempnum == lid[0])
 				{
-					ic++;
+					ic[0]++;
 					state = 0;
 				}
 				else
 				{
-					branchreport(bc, ic);
+					branchreport(bc-1, ic[0]);
+					branchreport(bc, ic[1]);
 					bc++;
 					lid[0] = tempnum;
 					flag--;
-					ic = 1;
+					ic[0] = 1;
+					ic[1] = 0;
 					state = 0;
 				}
 			
 			}
+		prediction1 = bit1(prediction1, state, out1);
+		//prediction2 = bit2(prediction2, state);
 		}
 		if (init == 0)
 		{ 
 			lid[0] = tempnum;
 			init = 1;
 			bc++;
-			ic++;
+			ic[0]++;
 		}
-	//	prediction1 = bit1(prediction1, state);
-	//	prediction2 = 2bit(prediction2, state);
+		printf("%lu\n", tempnum);
+		
 	}
 	fclose(input1);
 	printf("Completed.");
